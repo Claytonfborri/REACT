51d2c153bcf9c806b79619cbb5993a6a0ee4fd73
Clayton <clayton.borri18@gmail.com>
2022-08-01 13:12:56 -0300

    Estudo REACT, HOOKS, CONTEXT

---
+++ pokedex/src/componentes/SearchBar.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+
+const SearchBar = (props) => {
+    const [search, setSearch] = useState('dito')
+    const {onSearch} = props
+    const onChangeHandler = (e) => {
+        setSearch(e.target.value)
+        if(e.target.value.length === 0){
+            onSearch(undefined)
+        }
+    }
+
+    const onButtonClickHandler = () => {
+        onSearch(search)
+    }
+
+
+
+
+    return(
+        <div className="searchbar-container">
+            <div className="searchbar">
+                <input placeholder="Buscar pokemon" onChange={onChangeHandler}></input>
+            </div>
+            <div className="searchbar-btn">
+                <button onClick={onButtonClickHandler}>Buscar</button>
+            </div>
+        </div>
+    )
+}
+
+export default SearchBar;
