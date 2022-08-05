e848f3cdacd269f5c032a8c94d26de9a734f3483
Clayton <clayton.borri18@gmail.com>
2022-08-05 15:46:57 -0300

    Projeto exemplo para portifolio

---
+++ portifolio/src/components/ProjectsContainer.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const ProjectsContainer = () => {
+  return (
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Recusandae
+        nemo porro esse iste architecto possimus quae est soluta reiciendis
+        cupiditate impedit pariatur animi placeat, nam fuga, illum temporibus
+        sit maiores.
+      </p>
+      <a href="#"className="btn">
+        Ver Projetos
+      </a>
+    </section>
+  );
+};
+
+export default ProjectsContainer;
