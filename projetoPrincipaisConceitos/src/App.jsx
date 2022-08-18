14cfa086f2bb8d55f7769d1e5be40ed8af5f7d6f
Clayton <clayton.borri18@gmail.com>
2022-08-18 16:00:59 -0300

    Desenvolvendo

--- projetoPrincipaisConceitos/src/App.jsx
+++ projetoPrincipaisConceitos/src/App.jsx
@@ -1,15 +1,31 @@
+import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
 
-import './App.css'
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
+import NewProject from "./components/pages/NewProject";
+import Projects from "./components/pages/Projects";
+
+import Container from "./components/layout/Container";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
 
 function App() {
-
-
   return (
-    <div className="App">
-      <p>olá</p>
-        
-    </div>
-  )
-}
+    <Router>
+      <Navbar />
+      <Container customClass="min_height">
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/company" element={<Company />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/newproject" element={<NewProject />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </Container>
 
-export default App
+      <Footer />
+    </Router>
+  );
+}
+export default App;
---
+++ projetoPrincipaisConceitos/src/components/pages/NewProject.jsx
@@ -0,0 +1,16 @@
+
+import ProjectForm from '../project/ProjectForm'
+
+import styles from './NewProject.module.css'
+
+const NewProject = () => {
+    return (
+        <div className={styles.newproject_container }>
+            <h1>Criar Projeto</h1>
+            <p>Crie seu projeto para depois adicionar o serviços</p>
+            <ProjectForm btnText="Criar Projeto"/>
+        </div>
+    )
+}
+
+export default NewProject
