7c29e2695e909a9388f37884941fb0d9c3393f90
Clayton <clayton.borri18@gmail.com>
2022-08-22 15:55:07 -0300

    Continuação do projeto

--- projetoPrincipaisConceitos/src/components/form/Select.jsx
+++ projetoPrincipaisConceitos/src/components/form/Select.jsx
@@ -1,14 +1,21 @@
+import styles from "./Select.module.css";
 
-
-import styles from './Select.module.css'
-
-
-const Select  = ({ text, name, options, handleOnChange, value }) => {
+const Select = ({ text, name, options, handleOnChange, value }) => {
   return (
     <div className={styles.form_control}>
       <label htmlFor={name}>{text}:</label>
-      <select name={name} id={name}>
+      <select
+        name={name}
+        id={name}
+        onChange={handleOnChange}
+        value={value || ""}
+      >
         <option>Selecione uma opção</option>
+        {options.map((option) => (
+          <option id={option.id} key={option.id}>
+            {option.name}
+          </option>
+        ))}
       </select>
     </div>
   );
